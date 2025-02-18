420c699048e869faa0a4ba73259d9df5ef3b0bac
Nidhi Patel <patelnidhim14@gmail.com>
2025-02-18 00:29:43 -0500

    completed

--- DecodeString.java
+++ DecodeString.java
@@ -54,3 +54,63 @@ class Solution {
         return currStr.toString();
     }
 }
+
+// Recursive solution
+class Solution {
+    // Declare i globally because we want the old value of i whenever a recursive
+    // call is made, we dont want it to initialize it to 0 again
+    int i;
+
+    public String decodeString(String s) {
+        // Base case
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        // Declare 2 variables, because we want our currStr and num to reset to 0 on
+        // every recursive call, because old value will be stored in stack
+        StringBuilder currStr = new StringBuilder();
+        int num = 0;
+        // Iterate till the length of string
+        while (i < s.length()) {
+            // Take one char at a time
+            char c = s.charAt(i);
+            // If digit, simply get it's value in num and increment i
+            if (Character.isDigit(c)) {
+                i++;
+                num = num * 10 + c - '0';
+            }
+            // Whenever opening bracket, we want stack.push so recursive call will do that
+            // for us
+            else if (c == '[') {
+                i++;
+                // Recursive call
+                String decoded = decodeString(s);
+                // After the call is completed, our latest string will be stored in decoded and
+                // currstr and num will get values due to stack.pop
+                // Declare a new str
+                StringBuilder newStr = new StringBuilder();
+                // Since num is having the value of stack.pop, we want our decoded string to be
+                // repeated that many times
+                for (int i = 0; i < num; i++) {
+                    newStr.append(decoded);
+                }
+                // Then whatever value of currstr from stack, append to it the value of newstr
+                currStr.append(newStr);
+                // Set num to 0
+                num = 0;
+            } else if (c == ']') {
+                // If closing bracket, simply return the value of currstr and complete the
+                // recursive call, since we want stack.pop
+                i++;
+                return currStr.toString();
+            } else {
+                // If char, simply append to currStr
+                i++;
+                currStr.append(c);
+            }
+        }
+        // Return currStr
+        return currStr.toString();
+
+    }
+}
